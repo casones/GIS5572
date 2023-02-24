84fdc4694a0659efa1cd9fbd2b71f7b8c00def64
casones <113566302+casones@users.noreply.github.com>
2023-02-23 22:46:41 -0600

    ETL Pipeline Jupyter Notebook and app1.py

--- Lab1/app1.py
+++ Lab1/app1.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 @app.route('/get_polygon', methods=['GET'])
 def get_polygon():
     connection= psycopg2.connect(host = '34.135.163.144',
-                             database = 'lab1',
-                             user = 'postgres',
-                             password = 'mgispostgres')
+                                 database = 'lab1',
+                                 user = 'postgres',
+                                 password = 'mgispostgres')
 
     cursor = connection.cursor(cursor_factory=RealDictCursor)
-    cursor.execute(sql.SQL("SELECT poly, ST_AsGeoJSON(polygon)::json AS geometry FROM poly"), (1,))
+    cursor.execute(sql.SQL("SELECT poly, ST_AsGeoJSON(geom)::json AS geometry FROM poly"), (1,))
     result = cursor.fetchone()
     cursor.close()
     connection.close()
